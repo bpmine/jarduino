c614edad8cbb03f4c1ebd52b56766fb7475592ed
bernard <bernard@pinochet.fr>
2025-05-11 01:04:34 +0200

    Report du programme oya à partir de la carte arduino de l'an passé.

---
+++ oyas/slaves_rs485/carte_at328/slave485/pins.h
@@ -0,0 +1,20 @@
+#ifndef PINS_HEADER_INCLUDED
+#define PINS_HEADER_INCLUDED
+
+#define PIN_CPT_LVL_LOW			(9)
+#define PIN_CPT_LVL_HIGH		(10)
+#define PIN_CPT_FLOW			(8)
+#define PIN_TX_EN				(A5)
+#define PIN_CMD_EV				(7)
+#define PIN_LED					(18)
+
+#define PIN_MES_V				(A2)
+#define PIN_ADDR_A1				(3)
+#define PIN_ADDR_A2				(6)
+#define PIN_ADDR_A3				(4)
+#define PIN_ADDR_A4				(5)
+
+#define PIN_DGB_SYNC			(A0)      ///< @remark A 1 pendant les mesures (temp/hum)
+#define PIN_DGB_CMD				(12)      ///< @remark Change d'etat à chaque reception de commande
+
+#endif

3352877e9d0b9a4ca609dd7370254c1667ee5e99
bernard <bernard@pinochet.fr>
2025-05-11 00:42:36 +0200

    Test ok premier Oya en standalone

--- oyas/slaves_rs485/carte_at328/tests_bare/tests_bare.cpp
+++ oyas/slaves_rs485/carte_at328/tests_bare/tests_bare.cpp
@@ -1,9 +1,17 @@
 ﻿#include <Arduino.h>
 
+#define ENABLE_RS485
+
 void setup()
 {	
 	pinMode(A4,OUTPUT);
 	digitalWrite(A4,HIGH);
+	
+	#ifdef ENABLE_RS485
+		pinMode(A5,OUTPUT);
+		digitalWrite(A5,LOW);
+		Serial.begin(9600);
+	#endif
 }
 
 void loop()
@@ -11,7 +19,14 @@ void loop()
 	digitalWrite(A4,LOW);
 	delay(1000);
 	digitalWrite(A4,HIGH);
-	delay(1000);
+	#ifdef ENABLE_RS485
+		digitalWrite(A5,HIGH);
+		delay(20);
+		Serial.print("A");
+		delay(280);
+		digitalWrite(A5,LOW);
+	#endif
+	delay(700);
 }
 
 

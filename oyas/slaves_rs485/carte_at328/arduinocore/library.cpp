e1dd7a223025963747334a158dd41e4204c2c12e
bernard <bernard@pinochet.fr>
2025-05-10 21:01:02 +0200

    Création logiciel carte AT328

---
+++ oyas/slaves_rs485/carte_at328/arduinocore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 17/04/2025 22:50:02
+ * Author : Utilisateur
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ oyas/slaves_rs485/carte_at328/tests_bare/tests_bare.cpp
@@ -0,0 +1,17 @@
+﻿#include <Arduino.h>
+
+void setup()
+{	
+	pinMode(A4,OUTPUT);
+	digitalWrite(A4,HIGH);
+}
+
+void loop()
+{
+	digitalWrite(A4,LOW);
+	delay(1000);
+	digitalWrite(A4,HIGH);
+	delay(1000);
+}
+
+

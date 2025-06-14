10cd935775059f566180e9ba591ed8da64c6fed4
bernard <bernard@pinochet.fr>
2025-06-14 17:55:46 +0200

    Programmation Paul et Barbec

--- oyas/master_rs485/src/master_485/globals.h
+++ oyas/master_rs485/src/master_485/globals.h
@@ -6,8 +6,8 @@
 #ifndef GLOBALS_HEADER_INCLUDED
 #define GLOBALS_HEADER_INCLUDED
 
-//#define NODE_BARBEC
-#define NODE_REDUIT
+#define NODE_BARBEC
+//#define NODE_REDUIT
 //#define NODE_PAUL
 
 #if defined(NODE_BARBEC) && !defined(NODE_REDUIT) && !defined(NODE_PAUL)

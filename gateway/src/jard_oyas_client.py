48ada0173ab51869d198c46c5b45b564475b037a
bernard <bernard@pinochet.fr>
2025-07-05 17:08:55 +0200

    .

--- gateway/src/jard_oyas_client.py
+++ gateway/src/jard_oyas_client.py
@@ -4,6 +4,86 @@ import json
 import redis
 import time
 
+### {
+###   "on": false,
+###   "alive": false,
+###   "modules":
+###   {
+###      "barbec":
+###		 {
+###         "name":"barbec",
+###         "slaves":14,	    ///< 1 bit = 1 esclave present: 14 veut dire bit 2..4 présents donc esclaves 1, 2, 3 et 4 presents
+###         "cmds":6,			///< Même principe pour les commandes des EVs/Pompes (6 = 1 et 2 commandes)
+###         "ons":6,			///< Même principe pour les etats ON/OFF des EVs/Pompe (6 = 1 et 2 ON)
+###         "comms":14,			///< Même principe pour la comm (oyas/pompe communiquent avec le maitre ou pas)
+###         "lows":4,			///< Même principe pour le capteur bas de chaque oya (4 = oya 2 low tous les autres pas low)
+###         "highs":0,			///< Même principe pour le capteur haut de chaque oya (0 = aucun oya high)
+###         "date_mqtt":"2025-05-14T00:45:12Z",
+###         "date":"2025-05-14T00:46:12Z",
+###         "slaves":
+###         [
+###           {
+###              "addr":1,
+###              "type":"pump",
+###              "on":true,
+###				 "flow":456,
+###              "comm_ok":true
+###           },
+###           {
+###              "addr":2,
+###              "type":"oya",
+###              "on":true,
+###              "low":true,
+###              "high":false,
+###              "comm_ok":true
+###           },
+###           {
+###              "addr":3,
+###              "type":"oya",
+###              "on":false,
+###              "low":false,
+###              "high":false,
+###              "comm_ok":true
+###           }
+###         ]
+###		 },
+###      "reduit":
+###		 {
+###         "name":"reduit",
+###         "slaves":2,
+###         "cmds":0,
+###         "ons":0,
+###         "comms":2,
+###         "lows":0,
+###         "highs":0,
+###         "date_mqtt":"2025-05-14T00:45:12Z",
+###         "date":"2025-05-14T00:46:12Z",
+###         "slaves":
+###         [
+###           {
+###              "addr":1,
+###              "type":"pump",
+###              "on":false,
+###				 "flow":0,
+###              "comm_ok":true
+###           }
+###         ]
+###		 },  
+###   }
+### }
+###
+###
+### Il y'a plusieurs maitres des oyas (objet reduit:{} et barbec:{}). On en aura trois avec trois noms differents
+###
+### Ne pas decoder cmds, ons, lows et highs dont le detail est repris dans la liste slaves:[]
+###
+### La liste slaves[] est variable et contient tous les esclaves.
+### NB: Il y'a soit une pompe, soit des oyas dans cette liste. L'IHM doit s'adapter...
+###
+
+
+
+
 class RdOyasClient(RdApp):
     def __init__(self,ip,port=6379):
         super(RdOyasClient,self).__init__(ip,port)
